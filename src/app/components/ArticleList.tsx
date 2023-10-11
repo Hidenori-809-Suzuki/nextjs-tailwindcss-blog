623f29307b87df16fe11f4dd53f31991cf27266c
Hidenori_809 Suzuki <suzuki809hidenori@gmail.com>
2023-10-11 17:01:54 +0900

    section4-finished

---
+++ src/app/components/ArticleList.tsx
@@ -0,0 +1,74 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const ArticleList = () => {
+  return (
+    <div>
+      <article className="shadow my-4">
+        <Link href="#" className="hover:opacity-75">
+          <Image
+            src="https://source.unsplash.com/collection/1346951/1000x500?sig=1"
+            alt=""
+            width={1280}
+            height={300}
+          />
+        </Link>
+        <div className="bg-white flex flex-col justify-start p-6">
+          <Link href="#" className="text-blue-700 pb-4 font-bold">
+            Technology
+          </Link>
+          <Link
+            href="#"
+            className="text-slate-900 text-3xl font-bold hover:text-gray-700 pb-4"
+          >
+            Next.jsの勉強中
+          </Link>
+          <p className="text-sm pb-3 text-slate-900">Published on 2023/10/11</p>
+          <Link href="#" className="text-slate-900 pb-6">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Consequuntur dolorem est quos repudiandae ratione alias nostrum
+            quibusdam facilis. Accusantium explicabo fugiat, maxime quisquam
+            ipsum odio quia accusamus quidem. Optio, soluta.
+          </Link>
+          <Link href="#" className="text-pink-800 hover:text-black">
+            続きを読む
+          </Link>
+        </div>
+      </article>
+      <article className="shadow my-4">
+        <Link href="#" className="hover:opacity-75">
+          <Image
+            src="https://source.unsplash.com/collection/1346951/1000x500?sig=2"
+            alt=""
+            width={1280}
+            height={300}
+          />
+        </Link>
+        <div className="bg-white flex flex-col justify-start p-6">
+          <Link href="#" className="text-blue-700 pb-4 font-bold">
+            Technology
+          </Link>
+          <Link
+            href="#"
+            className="text-slate-900 text-3xl font-bold hover:text-gray-700 pb-4"
+          >
+            Next.jsの勉強中
+          </Link>
+          <p className="text-sm pb-3 text-slate-900">Published on 2023/10/11</p>
+          <Link href="#" className="text-slate-900 pb-6">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Consequuntur dolorem est quos repudiandae ratione alias nostrum
+            quibusdam facilis. Accusantium explicabo fugiat, maxime quisquam
+            ipsum odio quia accusamus quidem. Optio, soluta.
+          </Link>
+          <Link href="#" className="text-pink-800 hover:text-black">
+            続きを読む
+          </Link>
+        </div>
+      </article>
+    </div>
+  );
+};
+
+export default ArticleList;
